ebf8e51f966340082e7f05048e1e377de2a0a908
akibnayan <akibnayan182@gmail.com>
2026-05-02 16:26:20 +0600

    advanced oauth part done

--- oauth2_scopes/main.py
+++ oauth2_scopes/main.py
@@ -138,3 +138,39 @@ async def get_current_active_user(
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password"
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username, "scope": " ".join(form_data.scopes)},
+        expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+) -> User:
+    return current_user
+
+
+@app.get("/users/me/items")
+async def read_own_items(
+    current_user: Annotated[User, Security(get_current_active_user, scopes=["items"])]
+):
+    return [{"item_id": "Foo", "Owner": current_user.username}]
+
+@app.get("/status")
+async def read_system_status(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    return {"status": "Ok"}
